8c0c0dc1028ea42f343fc876983ff386985307d0
laurnichitean <nichitean@gmail.com>
2015-01-09 07:53:49 +0000

    tests for jobs get and refactored

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var jobModel = require("./models/Job.js");
+var jobsData = require("./jobs-data.js");
 
 var app = express();
 
@@ -7,8 +9,21 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res) {
-   res.render('index'); 
+app.get('/api/jobs', function(req, res) {
+    jobsData.findJobs().then(function(collection) {
+        res.send(collection);
+    });
 });
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+jobsData.connectDB('mongodb://laur:bv802324@ds031661.mongolab.com:31661/jobfinder')
+    .then(function() {
+        console.log('connected to mongodb successfully!');
+        jobModel.seedJobs();
+    });
 
 app.listen(process.env.PORT, process.env.IP);
